a15482c5488a3edbde0abfeaea7cd009e5db05eb
ksagameng <fordealisbad@gmail.com>
2026-09-16 21:19:58 +0300

    scripts: gha: update ios build scripts

--- scripts/gha/build_ios.sh
+++ scripts/gha/build_ios.sh
@@ -4,9 +4,7 @@
 
 cd "$GITHUB_WORKSPACE" || die
 
-./waf configure --enable-lto --ios build install --destdir=build/ios || die_configure
-
-cp -vr /Library/Frameworks/SDL2.framework ./build
+cp -vr /Library/Frameworks/SDL2.framework ./
 
 pushd hlsdk || die
 mkdir -p ../build/ios/libs || die
@@ -14,7 +12,14 @@ cmake -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 -DCMAKE_INSTALL
 cmake --build build --target install || die
 popd || die
 
-./scripts/ios/createipa.sh
+cd ios || die
+xcodebuild -scheme Launcher -target Launcher build -configuration Release \
+    CODE_SIGNING_ALLOWED=NO \
+    CODE_SIGNING_REQUIRED=NO \
+    CODE_SIGN_IDENTITY="" \
+    CODE_SIGN_ENTITLEMENTS="" || die
+
+cd .. || die
 
 mkdir -p artifacts/
 mv "build/xash3d.ipa" "artifacts/xash3d-fwgs-ios-arm64.ipa"
